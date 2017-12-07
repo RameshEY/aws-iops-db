2bdcccb9621738a5e846487ac6ffd2cf589fa382
Kevin Liu <kliu@kenzan.com>
2017-12-06 23:24:43 -0500

    Revert "adding YCSB and cassandra scripts"
    This reverts commit 84a07f995e547dcdc1aaa114a8f5a0ee5faf60d9.

--- terraform-YCSB/install_YCSB.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get -y install default-jre
-sudo apt-get -y install default-jdk
-sudo ln -s /home/ubuntu/ycsb-0.12.0/bin/ycsb.sh /etc/alternatives/ycsb
-sudo ln -s /etc/alternatives/ycsb /usr/bin/ycsb
