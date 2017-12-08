e41cf12c6610ff5d1faa0007c977f3d8e48ee5f2
msettro <monjay.settro@gmail.com>
2017-12-08 15:45:12 -0500

    Revert "adding YCSB and Cassandra scripts with further cleanup"

--- terraform-YCSB/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_YCSB" {
-  value = "${aws_instance.web.public_ip}"
-}
--- terraform-YCSB/setup_YCSB.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-export YCSB_HOME=/home/ubuntu/ycsb-0.12.0
-echo $JAVA_HOME
-echo $YCSB_HOME
