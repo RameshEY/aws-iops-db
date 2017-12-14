013d65a0858565d6f505470572e5a1bb7f45b53e
Vivek.Singh <vsingh@kenzan.com>
2017-12-14 10:20:35 -0500

    more linux tuning for mongo

--- terraform-mongo/templates/user-data.sh
+++ terraform-mongo/templates/user-data.sh
@@ -55,8 +55,16 @@ echo never > /sys/kernel/mm/transparent_hugepage/defrag
 
 # virtual memory tuning
 #dirty ratio  and dirtybackground ratio change
-vm.swapiness=0
-#
+#vm.swapiness=0
+#filesytem changes suggested
+#enable the deadline scheduler
+#Make the readahead to 8K
+#echo noop > /sys/block/${DEVICE_NAME}/queue/scheduler
+#touch /var/lock/subsys/local
+#echo 0 > /sys/class/block/${DEVICE_NAME}/queue/rotational
+#echo 8 > /sys/class/block/${DEVICE_NAME}/queue/read_ahead_kb
+#Start the mongodb using interleaved-mode numactl--interleave=all in the mongodb.conf file
+
 
 # shorter keepalives, 120s recommended for MongoDB in official docs:
 # https://docs.mongodb.org/manual/faq/diagnostics/#does-tcp-keepalive-time-affect-mongodb-deployments
@@ -67,6 +75,7 @@ fs.file-max = 65536
 vm.dirty_ratio=15
 vm.dirty_background_ratio=5
 vm.swapiness=0
+vm.zone_reclaim_mode = 0
 EOF
 sysctl -p
 
