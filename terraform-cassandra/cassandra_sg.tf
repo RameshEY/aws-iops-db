deb055cfd1cf0f59747f80b7370ce4cd78032fc5
Monjay Settro <msettro@kenzan.com>
2017-12-15 18:15:42 -0500

    update cassandra_sg.tf to allow outbound all egress

--- terraform-cassandra/cassandra_sg.tf
+++ terraform-cassandra/cassandra_sg.tf
@@ -12,6 +12,16 @@ resource "aws_security_group" "database" {
       "0.0.0.0/0"
     ]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
 }
 
 // Allow any internal network flow.
