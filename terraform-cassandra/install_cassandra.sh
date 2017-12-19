3d497d7d37cce7da299adc2047162d13dc6c3aad
Kevin Liu <kliu@kenzan.com>
2017-12-19 17:19:18 -0500

    AI-70 debugging integration issues on the develop main branch

---
+++ terraform-cassandra/install_cassandra.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+apt_source='deb http://repos.azulsystems.com/debian stable main'
+apt_list='/etc/apt/sources.list.d/zulu.list'
+echo "$apt_source" | sudo tee "$apt_list" > /dev/null
+sudo apt-get update
+sudo apt-get install -y zulu-8
+sudo apt-get install -y python-pip
+sudo pip install cassandra-driver
+
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo apt-get update
+sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
+sudo apt-get install -y cassandra
+sudo apt-get install -y cassandra-tools
