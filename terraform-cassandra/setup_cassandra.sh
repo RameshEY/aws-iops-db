5bde3226a005ef82da965e30eda4e8f6709e3e07
Kevin Liu <kliu@kenzan.com>
2017-12-19 17:26:44 -0500

    AI-70 debugging integration issues on the develop main branch

--- terraform-cassandra/setup_cassandra.sh
+++ terraform-cassandra/setup_cassandra.sh
@@ -7,8 +7,8 @@ sudo rm -Rf /var/lib/cassandra/* /var/log/cassandra/*
 sudo cp -f /tmp/provisioning/cassandra.yaml /etc/cassandra/cassandra.yaml
 sudo service cassandra start
 
+sleep 60
 private_ip=$(hostname -I)
 nodetool -h ${private_ip} status
-sleep 60
 cat /tmp/provisioning/init_cassandra.cql | cqlsh ${private_ip}
 cqlsh ${private_ip} -e "describe keyspace ycsb"
